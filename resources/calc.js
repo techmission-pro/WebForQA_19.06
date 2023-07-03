ae619d7094eb70e98321cee88c41d061ad53370e
ChorpitaM <chorpita9876@gmail.com>
2023-07-03 20:51:41 +0200

    Calculator of two numbers with different operations

--- resources/calc.js
+++ resources/calc.js
@@ -1,8 +1,56 @@
 document.querySelector('.calc-calculate').addEventListener('click', function () {
 const num1 = +document.querySelector('[name="num1"]').value;
 const num2 = +document.querySelector('[name="num2"]').value;
+const op = document.querySelector('[name="op"]').value;
 
-const sum = num1 + num2;
+let result;
 
-document.querySelector('.calc-result').value = sum;
+if (op === 'plus') {
+	result = num1 + num2;
+} else if (op === 'minus') {
+	result = num1 - num2;
+} else if (op === 'multiply') {
+	result = num1 * num2;
+}else if (op === 'divide') {
+	result = num1 / num2;
+}
+	<style>
+document.querySelector('.calc-result').value = result;
 });
+/* Stylizacja przycisków */
+    .custom-button {
+      display: inline-block;
+      padding: 10px 20px;
+      font-size: 16px;
+      font-weight: bold;
+      text-align: center;
+      text-decoration: none;
+      background-color: #4CAF50;
+      color: #fff;
+      border: none;
+      border-radius: 5px;
+      cursor: pointer;
+      transition: background-color 0.3s ease;
+    }
+
+    .custom-button:hover {
+      background-color: #45a049;
+    }
+
+    .custom-button.primary {
+      background-color: #007bff;
+    }
+
+    .custom-button.primary:hover {
+      background-color: #0056b3;
+    }
+
+    .custom-button.secondary {
+      background-color: #6c757d;
+    }
+
+    .custom-button.secondary:hover {
+      background-color: #5a6268;
+    }
+	
+	</style>
