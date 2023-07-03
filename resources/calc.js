a27ffea905ba5088d499a703f37fca4c359b7917
ChorpitaM <chorpita9876@gmail.com>
2023-07-03 21:18:17 +0200

    Automatic update of the result in the calculator

--- resources/calc.js
+++ resources/calc.js
@@ -1,4 +1,4 @@
-document.querySelector('.calc-calculate').addEventListener('click', function () {
+function calculate() {
 const num1 = +document.querySelector('[name="num1"]').value;
 const num2 = +document.querySelector('[name="num2"]').value;
 const op = document.querySelector('[name="op"]').value;
@@ -14,43 +14,23 @@ if (op === 'plus') {
 }else if (op === 'divide') {
 	result = num1 / num2;
 }
-	<style>
-document.querySelector('.calc-result').value = result;
-});
-/* Stylizacja przycisków */
-    .custom-button {
-      display: inline-block;
-      padding: 10px 20px;
-      font-size: 16px;
-      font-weight: bold;
-      text-align: center;
-      text-decoration: none;
-      background-color: #4CAF50;
-      color: #fff;
-      border: none;
-      border-radius: 5px;
-      cursor: pointer;
-      transition: background-color 0.3s ease;
-    }
-
-    .custom-button:hover {
-      background-color: #45a049;
-    }
-
-    .custom-button.primary {
-      background-color: #007bff;
-    }
-
-    .custom-button.primary:hover {
-      background-color: #0056b3;
-    }
-
-    .custom-button.secondary {
-      background-color: #6c757d;
-    }
-
-    .custom-button.secondary:hover {
-      background-color: #5a6268;
-    }
 	
-	</style>
+document.querySelector('.calc-result').value = result;
+}
+
+document.querySelector('.calc-calculate').addEventListener('click', function () {
+	calculate();
+});
+
+document.querySelector('[name="op"]').addEventListener('change', function() {
+	calculate();
+});
+
+document.querySelector('[name="num1"]').addEventListener('input', function() {
+	calculate();
+});
+
+document.querySelector('[name="num2"]').addEventListener('input', function() {
+	calculate();
+});
+
