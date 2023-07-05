72ecee89f71ef2c47e749ee88ac59b5fe0052168
Olena <olenazubova165@gmail.com>
2023-07-05 20:04:50 +0300

    Advanted calculate plus

---
+++ calc.js
@@ -0,0 +1,30 @@
+function calculate() {
+const numl = +document.querySelector('[name="num1"]').value;
+const num2 = +document.querySelector('[name="num2"]').value;
+const op = document.querySelector('[name="op"]').value;
+
+let result;
+
+	if (op === 'plus') {
+	result = numl + num2;
+	} else if (op === 'minus') {
+	result = numl - num2;
+	} else if (op === 'multiply'){
+	result = numl * num2;
+	} else if (op === 'divide'){
+	result = numl / num2;
+	} else {
+	console.error('Wrong op!');
+	alert('Wrong operation is selected');
+}
+
+document.querySelector('.calc-result').value = result;
+}
+
+document.querySelector('.calc-calculate').addEventListener('click', function () {
+calculate();
+});
+
+document.querySelector('[name="op"]').addEventListener('change',function() {
+calculate();
+});
